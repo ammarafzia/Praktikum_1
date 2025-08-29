672df9592fe06d4b73441060625e8f6accb4fed2
ammarafzia <ammarafashazia@gmail.com>
2025-08-29 17:36:36 +0700

    no 1

---
+++ Gambar.java
@@ -0,0 +1,12 @@
+public class Gambar {
+    public static void main(String[] args) {
+        System.out.println("   .___,   ");
+        System.out.println("___('v')___");
+        System.out.println("`\"-\\._./-\"'");
+        System.out.println("   ^   ^   ");
+        System.out.println("---------------------------");
+        System.out.println("Ammara Fasha Zia \uD83E\uDD89 (NPM: 2306245844)");
+    }
+
+
+}
